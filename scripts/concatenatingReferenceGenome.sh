c8df8b97a9148431702b4d17accbf1ebf133a625
VilainLab <39493166+VilainLab@users.noreply.github.com>
2019-04-25 11:36:47 -0400

    Create concatenatingReferenceGenome.sh

---
+++ scripts/concatenatingReferenceGenome.sh
@@ -0,0 +1,3 @@
+###Concatenating 2 reference genomes
+cat /data/Suro/Fasta/Homo_sapiens.GRCh38.dna.primary_assembly.mod.fa /data/Suro/Sus_Scrofa/Sus_scrofa.Sscrofa11.1.dna.mod.toplevel.fa > /data/Suro/Sus_Scrofa/HumanPig/HS_Pig.fa
+cat /data/Suro/Fasta/gencode.v28.annotation_mod.chrhuman.gtf /data/Suro/Sus_Scrofa/Sus_scrofa.Sscrofa11.1.91.mod.gtf > /data/Suro/Sus_Scrofa/HumanPig/HS_Pig.gtf
